5206fed1a796343ac08d1c8ea8812a548e1015ae
sachin pande <123870032+codexsachin@users.noreply.github.com>
2024-05-23 14:20:46 +0530

    Update Navbar.js

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -14,10 +14,10 @@ const Navbar = () => {
       <span className="ml-3 text-xl">Tailblocks</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900">First Link</a>
-      <a className="mr-5 hover:text-gray-900">Second Link</a>
-      <a className="mr-5 hover:text-gray-900">Third Link</a>
-      <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+      <a href="" className="mr-5 hover:text-gray-900">12th pass jobs</a>
+      <a className="mr-5 hover:text-gray-900">it jobs</a>
+      <a className="mr-5 hover:text-gray-900">freshars jobs</a>
+      <a className="mr-5 hover:text-gray-900">others jobs</a>
     </nav>
     <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
