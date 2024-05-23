cfd724a0c8b61ed2a65ea20282e6dcbcb4631017
sachin pande <123870032+codexsachin@users.noreply.github.com>
2024-05-23 18:17:41 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -38,7 +38,7 @@ const page = () => {
                           class = "w-4 h-4 ml-2"
         viewBox = "0 0 24 24">
                           <path d="M5 12h14M12 5l7 7-7 7" > </path> </svg> </a> <
-        Link href="/No_2" > Apply here </Link>  </div> <div class="p-4 md:w-1/3 sm:mb-0 mb-6" >
+        Link href="/Newupdate" > Apply here </Link>  </div> <div class="p-4 md:w-1/3 sm:mb-0 mb-6" >
                                 <
         div class="rounded-lg h-64 overflow-hidden" >
                                   <
