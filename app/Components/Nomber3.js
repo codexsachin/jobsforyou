dc6e6a91ff665692aa1a57d947fda51271fae46d
sachin pande <123870032+codexsachin@users.noreply.github.com>
2024-05-23 17:47:01 +0530

    Create Nomber3.js

---
+++ app/Components/Nomber3.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Nomber3= () => {
+  return (
+    <>
+      <div className='p-14 m-12 items-center justify-between bg-slate-100'>
+        <div className='items-center'>
+          <h1 className='text-center text-4xl '> company | iEnergizer </h1>{" "}
+          <p className='text-center'> Posted by jobsforyou.ver </p>{" "}
+        </div>{" "}
+        <div>
+          <img
+            src='https://job4freshers.co.in/wp-content/uploads/2023/01/job4freshers-81.webp?ezimgfmt=ng%3Awebp%2Fngcb2%2Frs%3Adevice%2Frscb2-1'
+            alt=''
+          />
+        </div>{" "}
+        <div className='bg-white'>
+          <p className='text-center p-16'> </p>iEnergizer is a global provider
+          of Business Process Outsourcing(BPO) solutions from customer service,
+          collections, to chat, claims and investor relations.Our BPO
+          capabilities help companies improve their operations through cost -
+          effective, value - generating customer contact and Back - Office
+          Processing Centres.{" "}
+        </div>{" "}
+        {/* this is about capgemini */}{" "}
+        <div className='p-14 items-center justify-between'>
+          <h1 className='text-center text-2xl bold'> About this job: </h1>{" "}
+          <p className='text-center p-16'> CUSTOMER SUPPORT </p>{" "}
+        </div>{" "}
+        <div>
+          <a
+            href=''
+            class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block'>
+            Walk in interview{" "}
+          </a>
+        </div>
+        <br />
+        <p>Monday – Friday</p>
+        <br />
+        <p>Address:
+iEnergizer
+A-37, Gate No. 2,
+Sector-60,
+Noida, Uttar Pradesh, India</p>
+      </div>
+    </>
+  );
+};
+
+export default Nomber3;
