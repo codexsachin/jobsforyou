3d1463226a8e5f7ccda45e7c5f5d7c942cb1270c
sachin pande <123870032+codexsachin@users.noreply.github.com>
2024-05-23 17:53:14 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import  React from "react"
+import React from "react"
 import Link from "next/link"
 
 // import React, { useState } from 'react'
@@ -8,63 +8,107 @@ import Link from "next/link"
 const page = () => {
 
 
-  return (
-    <div>
-      <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col">
-      <div class="h-1 bg-gray-200 rounded overflow-hidden">
-        <div class="w-24 h-full bg-indigo-500"></div>
-      </div>
-      <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
-        <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">Latest jobs</h1>
-        <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p>
-       
-      </div>
-    </div>
-    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-      <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-        <div class="rounded-lg h-64 overflow-hidden">
-          <img alt="content" class="object-cover object-center h-full w-full" src="https://job4freshers.co.in/wp-content/uploads/2023/01/job4freshers-81.webp?ezimgfmt=ng%3Awebp%2Fngcb2%2Frs%3Adevice%2Frscb2-1"/>
-        </div>
-        <h2 class="text-xl font-medium title-font text-gray-900 mt-5">Shooting Stars</h2>
-        <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-        <a class="text-indigo-500 inline-flex items-center mt-3">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
-        <Link href="/Newupdate">Apply here</Link> 
-      </div>
-      <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-        <div class="rounded-lg h-64 overflow-hidden">
-          <img alt="content" class="object-cover object-center h-full w-full" src="https://job4freshers.co.in/wp-content/uploads/2023/09/ezgif.com-gif-maker-93.webp?ezimgfmt=rs:841x471/rscb2/ng:webp/ngcb2"/>
-        </div>
-        <h2 class="text-xl font-medium title-font text-gray-900 mt-5">The Catalyzer</h2>
-        <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-        <a class="text-indigo-500 inline-flex items-center mt-3">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
-      </div>
-      <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-        <div class="rounded-lg h-64 overflow-hidden">
-          <img alt="content" class="object-cover object-center h-full w-full" src="https://job4freshers.co.in/wp-content/uploads/2022/05/Copy-of-Copy-of-Copy-of-Pastel-Abstract-New-Blog-Instagram-Post-68.webp?ezimgfmt=ng%3Awebp%2Fngcb2%2Frs%3Adevice%2Frscb2-1"/>
-        </div>
-        <h2 class="text-xl font-medium title-font text-gray-900 mt-5">The 400 Blows</h2>
-        <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-        <a class="text-indigo-500 inline-flex items-center mt-3">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
-      </div>
-    </div>
-  </div>
-</section>
-    </div>
-  )
+  return (<
+        div >
+    <
+        section class="text-gray-600 body-font" >
+      <
+        div class="container px-5 py-24 mx-auto" >
+        <
+        div class="flex flex-col" >
+          <
+        div class="h-1 bg-gray-200 rounded overflow-hidden" >
+            <
+        div class="w-24 h-full bg-indigo-500" > < /div> <
+        /div> <
+        div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12" >
+                <
+        h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0" > Latest jobs < /h1> <
+        p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0" > Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu.Viral gochujang bitters dreamcatcher. < /p>
+
+                    <
+        /div> <
+        /div> <
+        div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4" >
+                      <
+        div class="p-4 md:w-1/3 sm:mb-0 mb-6" >
+                        <
+        div class="rounded-lg h-64 overflow-hidden" >
+                          <
+                            img alt="content"
+                            class="object-cover object-center h-full w-full"
+                            src="https://job4freshers.co.in/wp-content/uploads/2023/01/job4freshers-81.webp?ezimgfmt=ng%3Awebp%2Fngcb2%2Frs%3Adevice%2Frscb2-1" />
+                          <
+        /div> <
+        h2 class="text-xl font-medium title-font text-gray-900 mt-5" > full stack developer < /h2> <
+        p class="text-base leading-relaxed mt-2" > LLTS IT PVT LTD is a leading technology solutions provider dedicated to delivering innovative IT services to clients across various industries. We are committed to fostering a collaborative and growth-oriented work environment. Our team thrives on creativity, cutting-edge technology, and the drive to achieve excellence.< /p> <
+        a href="" class="text-indigo-500 inline-flex items-center mt-3" > About this job <
+                                  svg fill="none"
+                                  stroke="currentColor"
+                                  stroke - linecap = "round"
+                                stroke - linejoin = "round"
+                                stroke - width = "2"
+                                class = "w-4 h-4 ml-2"
+        viewBox = "0 0 24 24" >
+                                <
+        path d="M5 12h14M12 5l7 7-7 7" > < /path> <
+        /svg> <
+        /a> <
+        Link href="/No_2" > Apply here < /Link>  <
+        /div> <
+        div class="p-4 md:w-1/3 sm:mb-0 mb-6" >
+                                      <
+        div class="rounded-lg h-64 overflow-hidden" >
+                                        <
+                                          img alt="content"
+                                          class="object-cover object-center h-full w-full"
+                                          src="https://jobnow247.in/wp-content/uploads/2024/05/Full-STACK-DEVOLOPER4.jpg" />
+                                        <
+        /div> <
+        h2 class="text-xl font-medium title-font text-gray-900 mt-5" > full stack developer < /h2> <
+        p class="text-base leading-relaxed mt-2" > LLTS IT PVT LTD is a leading technology solutions provider dedicated to delivering innovative IT services to clients across various industries. We are committed to fostering a collaborative and growth-oriented work environment. Our team thrives on creativity, cutting-edge technology, and the drive to achieve excellence.< /p> <
+        a href="./No_2" class="text-indigo-500 inline-flex items-center mt-3" > About this job <
+                                                svg fill="none"
+                                                stroke="currentColor"
+                                                stroke - linecap = "round"
+                                              stroke - linejoin = "round"
+                                              stroke - width = "2"
+                                              class = "w-4 h-4 ml-2"
+        viewBox = "0 0 24 24" >
+                                              <
+        path d="M5 12h14M12 5l7 7-7 7" > < /path> <
+        /svg> <
+        /a> <
+        /div> <
+        div class="p-4 md:w-1/3 sm:mb-0 mb-6" >
+                                                  <
+        div class="rounded-lg h-64 overflow-hidden" >
+                                                    <
+                                                      img alt="content"
+                                                      class="object-cover object-center h-full w-full"
+                                                      src="https://jobnow247.in/wp-content/uploads/2024/05/Customer-Job.jpg" />
+                                                    <
+        /div> <
+        h2 class="text-xl font-medium title-font text-gray-900 mt-5" > HIRING FOR INTERNATIONAL PROCESS | CUSTOMER SUPPORT | NOIDA< /h2> <
+        p class="text-base leading-relaxed mt-2" > iEnergizer is inviting applications for the position of CUSTOMER SUPPORT Here you are given complete information about the job. Must read the complete article for complete information. < /p> <
+        a href="./Components/Nomber3.js" class="text-indigo-500 inline-flex items-center mt-3" > Learn More <
+                                                            svg fill="none"
+                                                            stroke="currentColor"
+                                                            stroke - linecap = "round"
+                                                          stroke - linejoin = "round"
+                                                          stroke - width = "2"
+                                                          class = "w-4 h-4 ml-2"
+        viewBox = "0 0 24 24" >
+                                                          <
+        path d="M5 12h14M12 5l7 7-7 7" > < /path> <
+        /svg> <
+        /a> <
+        /div> <
+        /div> <
+        /div> <
+        /section> <
+        /div>
+                                                            )
 }
 
-export default page
+                                                            export default page
